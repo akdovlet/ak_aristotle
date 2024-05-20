72b8540cb8922ffa37593d1b41291f5eb6202bd8
Akhmed Dovletov <akdovlet@student.42.fr>
2024-05-20 20:53:05 +0200

    Today was a good day

---
+++ philo/include/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akdovlet <akdovlet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 19:47:52 by akdovlet          #+#    #+#             */
+/*   Updated: 2024/05/20 20:05:48 by akdovlet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h> // usleep, write
+# include <string.h> // memset
+# include <stdlib.h> // malloc, free
+# include <sys/time.h> // gettimeofday
+# include <pthread.h> 	// pthread_create, pthread_detach, pthread_join
+// pthread_mutex_init, pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
+# include <stdio.h> // printf
+#endif
